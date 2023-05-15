b5763a5616c9ff2a24306e6baf42a0513afa7da0
sami rhimi <samrhim@users.noreply.github.com>
2023-05-15 12:05:07 +0100

    Update managed.sh

--- managed.sh
+++ managed.sh
@@ -2,9 +2,8 @@
 
 echo "Managed Node Preparation ..."
 
-yum makecache --refresh
 yum update -y
-yum install -y python39 bind-utils libX11 
+# yum install -y python39 bind-utils libX11 
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
@@ -12,11 +11,11 @@ systemctl restart sshd
 PASS=$(echo "ansible" | openssl passwd -1 -stdin)
 useradd -p "$PASS" ansible
 cat <<EOF > /etc/sudoers.d/ansible
-ansible ALL=NOPASSWD: ALL
+ansible ALL=(ALL)   NOPASSWD: ALL
 EOF
 
-cat <<EOF > /etc/hosts
-192.168.30.200 master.clevory.local
-192.168.30.201 node1.clevory.local
-192.168.30.202 node2.clevory.local
-EOF
+# cat <<EOF > /etc/hosts
+# 192.168.30.200 master.clevory.local
+# 192.168.30.201 node1.clevory.local
+# 192.168.30.202 node2.clevory.local
+# EOF
